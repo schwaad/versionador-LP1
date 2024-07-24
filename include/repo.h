5b249e698ed4d91e4b7d6ba7b96b4a424a14b430
schwaad <joseivoschwadea@gmail.com>
2024-07-24 15:34:17 -0300

    Funções adicionadas: createRepo(), accessRepo() e createCommit()

--- include/repo.h
+++ include/repo.h
@@ -11,8 +11,24 @@
 class Repo {
  public:
   std::string name;
+  std::string location;
   std::time_t creationDate;
   std::vector<Commit> commits;
+
+ public:
+  std::string getRepoName() { return name; };
+  std::time_t getRepoDate() { return creationDate; };
+  std::string getRepoLocation() { return location; }
+  std::vector<Commit> getRepoCommits() { return commits; };
+  void setRepoName(std::string newname) { name = newname; };
+  void setRepoDate(std::time_t newdate) { creationDate = newdate; };
+  void setRepoLocation(std::string repoLocation) { location = repoLocation; };
+
+  void addCommit(Commit addedCommit) { commits.push_back(addedCommit); }
 };
+
+Repo createCommit(Repo repositorie);
+Repo createRepo();
+Repo accessRepo();
 
 #endif
