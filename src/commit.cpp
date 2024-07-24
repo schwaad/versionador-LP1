0640c6bba2740bbcf0d72fb9407ba3df3e70a3f6
schwaad <joseivoschwadea@gmail.com>
2024-07-24 15:36:53 -0300

    Retirada a função createCommit()

--- src/commit.cpp
+++ src/commit.cpp
@@ -4,38 +4,3 @@
 #include <iostream>
 
 #include "../include/utils.h"
-
-namespace fs = std::filesystem;
-
-Commit createCommit() {
-  Commit newCommit;
-  int choice;
-
-  do {
-    choice = commitMenu();
-
-    switch (choice) {
-      case 1:  // 1 = Adicionar arquivos ao commit
-        newCommit.addFileToCommit();
-        clearInputBuffer();
-        pauseProgram();
-        break;
-
-      case 2:  // 2 = Remover arquivos do commit
-        std::cout << "Ainda vou implementar" << "\n";
-        clearInputBuffer();
-        pauseProgram();
-        break;
-
-      case 3:  // 3 = Encerrar o Commit
-        std::cout << "Insira a mensagem do commit: ";
-        std::string str;
-        std::cin >> str;
-        newCommit.setCommitMessage(str);
-        break;
-    }
-
-  } while (choice != 3);
-
-  return newCommit;
-}
