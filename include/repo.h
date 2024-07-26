3281e5e54b53f62240b4a6f08786ec90edd8b11b
schwaad <joseivoschwadea@gmail.com>
2024-07-26 15:10:30 -0300

    Implementadas as funções removeRepo e checkRepoStatus. Além disso, agora a data de criação de um repositório é tratada como string.

--- include/repo.h
+++ include/repo.h
@@ -12,16 +12,16 @@ class Repo {
  public:
   std::string name;
   std::string location;
-  std::time_t creationDate;
+  std::string creationDate;
   std::vector<Commit> commits;
 
  public:
   std::string getRepoName() { return name; };
-  std::time_t getRepoDate() { return creationDate; };
+  std::string getRepoDate() { return creationDate; };
   std::string getRepoLocation() { return location; }
   std::vector<Commit> getRepoCommits() { return commits; };
   void setRepoName(std::string newname) { name = newname; };
-  void setRepoDate(std::time_t newdate) { creationDate = newdate; };
+  void setRepoDate(std::string newdate) { creationDate = newdate; };
   void setRepoLocation(std::string repoLocation) { location = repoLocation; };
 
   void addCommit(Commit addedCommit) { commits.push_back(addedCommit); }
@@ -30,5 +30,7 @@ class Repo {
 Repo createCommit(Repo repositorie);
 Repo createRepo();
 Repo accessRepo();
+void removeRepo();
+void checkRepoStatus(Repo repositorie);
 
 #endif
