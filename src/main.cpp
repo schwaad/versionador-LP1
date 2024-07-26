2afb3fce63bf1390ef12b678f6f5463e5855f6f7
schwaad <joseivoschwadea@gmail.com>
2024-07-26 15:23:00 -0300

    Agora o menu da main.cpp está devidamente implementado.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,29 @@
 #include "../include/utils.h"
 
 int main() {
-  menu();
+  do {
+    switch (menu()) {
+      case 1:
+        clearInputBuffer();
+        createRepo();
+        clearInputBuffer();
+        pauseProgram();
+        break;
+      case 2:
+        clearInputBuffer();
+        repoMenu(accessRepo());
+        clearInputBuffer();
+        break;
+      case 3:
+        clearInputBuffer();
+        removeRepo();
+        clearInputBuffer();
+        pauseProgram();
+        break;
+      case 0:
+        std::cout << "Saindo...\n";
+        return 0;
+    }
+  } while (menu() != 0);
   return 0;
 }
