0743df4f4e791db5c012233ced414196be9f4d96
schwaad <joseivoschwadea@gmail.com>
2024-07-26 15:12:21 -0300

    Implementadas as funções de receber o nome, a data de criação e a quantidade de commits a partir do arquivo de informações do repositório. Implementadas as funções utilitárias: rewriteCommitCount, isRepo, getCurrentDate e filterDirPath. Implementado o menu do repositório acessado.

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #include <filesystem>
 #include <iostream>
 
+#include "repo.h"
+
 namespace fs = std::filesystem;
 
 #ifndef _UTILSH_
@@ -13,5 +15,11 @@ int commitMenu();
 void pauseProgram();
 bool isDirectory(const std::string& path);
 std::string getRepoNamefromInfos(fs::path infosPath);
-
+std::string getCommitCountfromInfos(fs::path infosPath);
+std::string getCreationDatefromInfos(fs::path infosPath);
+void rewriteCommitCount(fs::path filename, std::string newCommitCount);
+bool isRepo(const std::string& dirPath);
+void repoMenu(Repo repositorie);
+std::string getCurrentDate();
+std::string filterDirPath(const std::string& dirPath);
 #endif
